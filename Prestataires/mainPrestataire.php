b2c0e47c0116db8e39036960e9a75e832ac89145
toupou <tapamedinadia@gmail.com>
2017-03-19 00:27:20 +0100

    la page php du main des prestataires

---
+++ Prestataires/mainPrestataire.php
@@ -0,0 +1,66 @@
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html lang="en">
+<head>
+	<title>Goorgoorlu</title>
+	<link rel="stylesheet" type="text/css" href="../css/style1.css"/>
+	<link rel="stylesheet" type="text/css" href="../css/styleMainPrestataire.css"/>
+	<!--[if lt IE 7]>
+	<link rel="stylesheet" href="ie.css" type="text/css">
+<![endif]-->
+</head>
+<body>
+<div id="wrap">
+
+	<div id="header">
+		<p><img src="images/logo.png" style="float: right;margin: 0.5em 2em;"></p>
+	</div>
+
+	<div id="content-wrap">
+		<div id="content">
+			
+			<div class="signIn  dividing">
+				<form class="field ">
+					<input type="text" name="" placeholder="login"/>
+					<input type="password" name="" placeholder="password"/>	
+					<button class="button">Connexion</button>
+				</form>
+			</div>
+			
+			<div class="signUp">
+			<h1>Inscription</h1>
+
+				<form class="fieldset">
+					
+						<h3>Compte</h3>
+
+						<label>Name</label>
+						<div style="padding: 1em 10em;">
+							<input type="text" name="" placeholder="nom">
+							<input type="text" name="" placeholder="prenom">	
+						</div>
+
+						<label>Contacts</label>	
+						<div style="padding: 1em 10em;">
+							<input type="text" name="" placeholder="adresse">
+							<input type="number" name="" placeholder="Tel">
+						</div>
+						<div style=" margin-left: 5em;">
+							<button class="button">Valider</button>
+						</div>	
+						
+				</form>
+			</div>
+
+		</div>
+	</div>
+
+	<div id="footer">
+		<div id="labfooter">
+			<p><a href="http://www.456bereastreet.com/lab/">a propos</a> | <a href="http://www.456bereastreet.com/">Conditions d'utilisations</a> | <a href="http://www.456bereastreet.com/lab/">confidentialite</a> </p>
+		</div>	
+	</div>
+
+</div>
+</body>
+</html>
