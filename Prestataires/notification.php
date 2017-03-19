80cffb267ed731524a93830f4c79d19f5eff856a
toupou <tapamedinadia@gmail.com>
2017-03-19 04:41:36 +0100

    page php notification

---
+++ Prestataires/notification.php
@@ -0,0 +1,114 @@
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html lang="en">
+<head>
+	<title>Goorgoorlu</title>
+	<link rel="stylesheet" type="text/css" href="../css/style1.css"/>
+	<link rel="stylesheet" type="text/css" href="../css/notification.css">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" type="text/css">
+	<!--[if lt IE 7]>
+	<link rel="stylesheet" href="ie.css" type="text/css">
+<![endif]-->
+</head>
+<body>
+<div id="wrap">
+
+	<div id="header">
+		<p><img src="images/logo.png" style="float: right;margin: 0.5em 2em;"></p>
+	</div>
+
+	<div id="content-wrap">
+			<div class="nav-side-menu">
+			    <div class="brand">Menu</div>
+			    <i class=" material-icons toggle-btn" data-toggle="collapse" data-target="#menu-content">reorder</i>
+			  
+			        <div class="menu-list">
+			            <ul id="menu-content" class="menu-content collapse out">
+			                <li >
+			                  <a href="#">
+			                  	 <i class="material-icons"> account_circle</i> 
+			                  	 <span>Account</span>   	 
+			                  </a>
+			                </li>
+			                <li data-toggle="collapse" class="collapsed active">
+			                	<a href="#">
+			                  	 <i class="material-icons"> announcement</i>
+			                  	 <span>Notifications</span>
+			                   </a>
+			                </li>  
+			                <li data-toggle="collapse"  class="collapsed">
+			                  <a href="#">	
+			                    <i class="material-icons">assessment</i>
+			                    <span>Statistiques</span>
+			                   </a>			                   
+			                </li>
+			                 <li data-toggle="collapse" class="collapsed">
+		                  		<a href="#"><i class="material-icons">settings</i>
+		                  			<span>Parametres</span>
+		                  		</a>
+			                </li>
+			            </ul>
+			    	</div>
+			</div>
+
+			<div id="content">
+				<div class="fieldset1" style="float: right;">
+					<img src="../images/service.png" style="padding-left: 36em;">
+				</div>
+				<div class="fieldset" style="float: right;">
+					
+					<div class="cards">
+						<div class="card">
+							<div class="content" style="padding: 1em;">
+								<div class="header">Nom Utilisateur</div>
+								<div class="meta">Date</div>
+								<div class="description">
+									Description du service rendu<br>
+									Note :
+								</div>
+							</div>
+						</div>
+						<div class="card">
+							<div class="content" style="padding: 1em;">
+								<div class="header">Nom Utilisateur</div>
+								<div class="meta">Date</div>
+								<div class="description">
+									Description du service rendu<br>
+									Note :
+								</div>
+							</div>
+						</div>
+						<div class="card">
+							<div class="content" style="padding: 1em;">
+								<div class="header">Nom Utilisateur</div>
+								<div class="meta">Date</div>
+								<div class="description">
+									Description du service <br> rendu
+									Note :
+								</div>
+							</div>
+						</div>
+						<div class="card">
+							<div class="content" style="padding: 1em;">
+								<div class="header">Nom Utilisateur</div>
+								<div class="meta">Date</div>
+								<div class="description">
+									Description du service<br> rendu
+									Note :
+								</div>
+							</div>
+						</div>
+					</div>
+				
+			</div>
+	</div>
+
+	<div id="footer">
+		<div id="labfooter">
+			<p><a href="http://www.456bereastreet.com/lab/">a propos</a> | <a href="http://www.456bereastreet.com/">Conditions d'utilisations</a> | <a href="http://www.456bereastreet.com/lab/">confidentialite</a> </p>
+		</div>	
+	</div>
+
+</div>
+</body>
+</html>
