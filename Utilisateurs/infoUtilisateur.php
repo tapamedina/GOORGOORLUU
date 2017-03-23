cdfb503e1615ae5e02836e74a414fd98d9f7b5b0
toupou <tapamedinadia@gmail.com>
2017-03-23 03:57:30 +0100

    ou le user saisie son nom et prenom

---
+++ Utilisateurs/infoUtilisateur.php
@@ -0,0 +1,45 @@
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html lang="en">
+<head>
+	<title>Goorgoorlu</title>
+	<link rel="stylesheet" type="text/css" href="../css/style1.css"/>
+	<link rel="stylesheet" type="text/css" href="../css/styleMainPrestataire.css"/>
+		
+	<!--[if lt IE 7]>
+	<link rel="stylesheet" href="ie.css" type="text/css">
+<![endif]-->
+</head>
+<body>
+<div id="wrap">
+
+	<div id="header">
+		<p><img src="images/logo.png" style="float: right;margin: 0.5em 2em;"></p>
+	</div>
+
+	<div id="content-wrap">
+		<div id="content">
+			<div class="fieldset" style="margin-top: 15em;">
+				<label>Name</label>
+					<div style="padding: 1em 10em;">
+						<div style="padding: 1em 10em;">
+							<input type="text" name="" placeholder="nom">
+						</div>
+						<div style="padding: 1em 10em;">
+							<input type="text" name="" placeholder="prenom">
+						</div>
+						<button class="button" style="margin-top: 2em; margin-left: 11.5em;">Connexion</button>	
+					</div>	
+			</div>		
+		</div>
+	</div>
+
+	<div id="footer">
+		<div id="labfooter">
+			<p><a href="http://www.456bereastreet.com/lab/">a propos</a> | <a href="http://www.456bereastreet.com/">Conditions d'utilisations</a> | <a href="http://www.456bereastreet.com/lab/">confidentialite</a> </p>
+		</div>	
+	</div>
+
+</div>
+</body>
+</html>
