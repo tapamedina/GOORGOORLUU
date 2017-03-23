02534754729a74d7ccf37236eda4b9c36cbd59bd
toupou <tapamedinadia@gmail.com>
2017-03-23 03:55:40 +0100

    page qui affiche la liste de tous les prestataires dispo d'un service

---
+++ Utilisateurs/listePrestataire.php
@@ -0,0 +1,96 @@
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html lang="en">
+<head>
+	<title>Goorgoorlu</title>
+	<link rel="stylesheet" type="text/css" href="../css/style1.css"/>
+	<link rel="stylesheet" type="text/css" href="../css/listePrestataires.css"/>
+	<!--[if lt IE 7]>
+	<link rel="stylesheet" href="ie.css" type="text/css">
+<![endif]-->
+</head>
+<body>
+<div id="wrap">
+
+	<div id="header">
+		<p><img src="images/logo.png" style="float: right;margin: 0.5em 2em;"></p>
+	</div>
+
+	<div id="content-wrap">
+		<div id="content">
+				<div>
+					<p style="margin-left:40rem;"><img src="../image/logoService.png"></p>
+				</div>
+				<div class="fieldset1">
+					   <div class="field">
+                  <div class="special cards">
+                      <div class="card">
+                        <div class=" image">
+                          <img src="../images/2.jpg">
+                        </div>
+                        <div class="content">
+                          <a class="header">Nom Prestataire</a> 
+                           <a class="header"> Moyenne</a>
+                          <a href="detailsPrestataire.php">details sur prestataire</a>
+                       </div>
+                        <div class="extra content">
+                         <input type="checkbox" name=""> Choisi
+                        </div>
+                      </div>
+                      <div class="card">
+                        <div class=" image">
+                          <img src="../images/2.jpg">
+                        </div>
+                        <div class="content">
+                          <a class="header">Nom Prestataire</a>
+                           <a class="header"> Moyenne</a>
+                          <a href="detailsPrestataire.php">details sur prestataire</a>
+                       </div>
+                        <div class="extra content">
+                         <input type="checkbox" name=""> Choisi
+                        </div>
+                      </div>
+                       <div class="card">
+                        <div class=" image">
+                          <img src="../images/2.jpg">
+                        </div>
+                        <div class="content">
+                          <a class="header">Nom Prestataire </a>
+                          <a class="header">Moyenne</a>
+                          <a href="detailsPrestataire.php">details sur prestataire</a>
+                       </div>
+                        <div class="extra content">
+                         <input type="checkbox" name=""> Choisi
+                        </div>
+                      </div>
+                      <div class="card">
+                        <div class=" image">
+                          <img src="../images/2.jpg">
+                        </div>
+                        <div class="content">
+                          <a class="header">Nom Prestataire</a>
+                          <a class="header"> Moyenne</a>
+                          <a href="datailsPrestataire.php">details sur prestataire</a>
+                       </div>
+                        <div class="extra content">
+                         <input type="checkbox" name=""> Choisi
+                        </div>
+                      </div>
+                  	</div>
+                  </div>
+                </div>
+                <div style=" margin-left: 60rem;margin-top: 0.8em;">
+                  <button class="button" style="width: 10em;"><a href="infoUtilisateur.php" style="color: white;">Valider</a></button>
+                </div>
+	       </div>
+      </div>
+    </div>
+	<div id="footer">
+		<div id="labfooter">
+			<p><a href="http://www.456bereastreet.com/lab/">a propos</a> | <a href="http://www.456bereastreet.com/">Conditions d'utilisations</a> | <a href="http://www.456bereastreet.com/lab/">confidentialite</a> </p>
+		</div>	
+	</div>
+
+
+</body>
+</html>
