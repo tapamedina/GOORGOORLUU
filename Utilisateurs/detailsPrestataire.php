15a075a4e8025e52097bfad0b576699b47b53063
toupou <tapamedinadia@gmail.com>
2017-03-23 03:58:53 +0100

    affiche les details sur un prestataire

---
+++ Utilisateurs/detailsPrestataire.php
@@ -0,0 +1,71 @@
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html lang="en">
+<head>
+	<title>Goorgoorlu</title>
+	<link rel="stylesheet" type="text/css" href="../css/style1.css"/>
+	<style type="text/css">
+		.fieldset1 {
+		  margin-left: 25em;
+		  margin-top: 1em;
+		  margin-bottom: 1em;
+		  padding: 0.35em 0.625em 0.75em;
+		  width: 75%;
+		  height: 35em;
+		  overflow-y: hidden;
+		}
+
+		.field {
+		  border: 1px solid #c0c0c0;
+		  margin-left: 1em;
+		  margin-top: 6em;
+		  margin-bottom: 1em;
+		  padding: 0.35em 0.625em 0.75em;
+		  width: 92%;
+		  height: 20em;
+		  overflow-x: hidden;
+		}
+
+	</style>
+	<!--[if lt IE 7]>
+	<link rel="stylesheet" href="ie.css" type="text/css">
+<![endif]-->
+</head>
+<body>
+<div id="wrap">
+	<div id="header">
+		<p><img src="images/logo.png" style="float: right;margin: 0.5em 2em;"></p>
+	</div>
+
+	<div id="content-wrap">
+		<div id="content">
+				<div>
+					<p style="margin-left:40rem;"><img src="../image/logoService.png"></p>
+				</div>
+				<div class="fieldset1" style="margin-left: 15em;">
+					<div class="dividing" style="margin-top: 5em;">	</div>
+					<div class="field" style="margin-left: 0;">
+					 	
+						<div class="content" style="margin-left: 2em;margin-top: 1.5em;line-height: 3em;font-size: 1.5em;">
+                          <a class="header">Nom Presta :</a><br>
+                          <a class="header">Prenom Presta :</a><br>
+                          <a class="header">Tel : </a><br>
+                          <a>Moyenne :</a>
+                       </div>	
+						
+					
+					</div>								
+			</div>
+
+</div>
+	</div>
+
+	<div id="footer">
+		<div id="labfooter">
+			<p><a href="http://www.456bereastreet.com/lab/">a propos</a> | <a href="http://www.456bereastreet.com/">Conditions d'utilisations</a> | <a href="http://www.456bereastreet.com/lab/">confidentialite</a> </p>
+		</div>	
+	</div>
+
+</div>
+</body>
+</html>
